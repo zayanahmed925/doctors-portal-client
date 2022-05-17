ad77cf2ea55dc2d8e1dd4066244aa68759b73b5a
zayanahmed925 <rjraju925@gmail.com>
2022-05-18 02:00:58 +0600

    jwt verify done

--- src/Pages/Dashboard/MyAppointment.js
+++ src/Pages/Dashboard/MyAppointment.js
@@ -1,19 +1,35 @@
+import { signOut } from 'firebase/auth';
 import React, { useState, useEffect } from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
+import { useNavigate } from 'react-router-dom';
 import auth from '../../firebase.init';
 const MyAppointment = () => {
     const [appointments, setAppointments] = useState([]);
     const [user, loading, error] = useAuthState(auth);
+    const navigate = useNavigate()
     useEffect(() => {
         if (user) {
-            fetch(`http://localhost:5000/booking?patient=${user.email}`)
-                .then(res => res.json())
+            fetch(`http://localhost:5000/booking?patient=${user.email}`, {
+                method: 'GET',
+                headers: {
+                    'authorization': `Bearer ${localStorage.getItem('accessToken')}`
+                }
+            })
+                .then(res => {
+                    console.log('res', res);
+                    if (res.status === 401 || res.status === 403) {
+                        signOut(auth);
+                        localStorage.removeItem('accessToken');
+                        navigate('/');
+                    }
+                    return res.json()
+                })
                 .then(data => setAppointments(data))
         }
     }, [user])
     return (
         <div>
-            <h2>My appointment {appointments.length}</h2>
+            <h2>My appointment {appointments?.length}</h2>
             <div class="overflow-x-auto">
                 <table class="table w-full">
                     {/* <!-- head --> */}
@@ -29,7 +45,7 @@ const MyAppointment = () => {
                     <tbody>
                         {/* <!-- row 1 --> */}
                         {
-                            appointments.map((a, index) => <tr>
+                            appointments?.map((a, index) => <tr>
                                 <th>{index + 1}</th>
                                 <td>{a.patient}</td>
                                 <td>{a.date}</td>
