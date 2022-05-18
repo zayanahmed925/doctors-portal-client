80ac4cf7e8b5362564511ea588e67026816c3347
zayanahmed925 <rjraju925@gmail.com>
2022-05-18 16:59:50 +0600

    make admin added with verify jwt

--- src/Pages/Dashboard/UserRow.js
+++ src/Pages/Dashboard/UserRow.js
@@ -1,13 +1,29 @@
 import React from 'react';
+import { toast } from 'react-toastify';
 
-const UserRow = ({ user }) => {
-    const { email } = user;
+const UserRow = ({ user, refetch }) => {
+    const { email, role } = user;
+    const makeAdmin = () => {
+        fetch(`http://localhost:5000/user/admin/${email}`, {
+            method: 'PUT',
+            headers: {
+                authorization: `Bearer ${localStorage.getItem('accessToken')}`
+            }
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                toast.success('Successfully make admin')
+                refetch()
+
+            })
+    }
     return (
 
         <tr>
             <th>1</th>
             <td>{email}</td>
-            <td><button class="btn btn-xs">Make Admin</button></td>
+            <td>{role !== 'admin' ? <button onClick={makeAdmin} class="btn btn-xs">Make Admin</button> : <h4 className='font-bold'>Already Admin</h4>}</td>
             <td><button class="btn btn-xs">Delete</button></td>
         </tr>
 
--- src/Pages/Dashboard/Users.js
+++ src/Pages/Dashboard/Users.js
@@ -4,7 +4,7 @@ import Loading from '../Shared/Loading/Loading';
 import UserRow from './UserRow';
 
 const Users = () => {
-    const { data: users, isLoading } = useQuery('users', () => fetch('http://localhost:5000/users', {
+    const { data: users, isLoading, refetch } = useQuery('users', () => fetch('http://localhost:5000/users', {
         method: 'GET',
         headers: {
             authorization: `Bearer ${localStorage.getItem('accessToken')}`
@@ -31,6 +31,7 @@ const Users = () => {
                             users.map(user => <UserRow
                                 key={user._id}
                                 user={user}
+                                refetch={refetch}
                             ></UserRow>)
                         }
                         {/* <!-- row 1 -->
