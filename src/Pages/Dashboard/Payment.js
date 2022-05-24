a6997ea08c4e58e3cf7da731be2defcbe71eae13
zayanahmed925 <rjraju925@gmail.com>
2022-05-25 02:28:18 +0600

    payment added

--- src/Pages/Dashboard/Payment.js
+++ src/Pages/Dashboard/Payment.js
@@ -1,11 +1,41 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import { useQuery } from 'react-query';
+import Loading from '../Shared/Loading/Loading';
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
+import CheckoutForm from './CheckoutForm';
 
+const stripePromise = loadStripe('pk_test_51L1W1gLCApDo5zACxxtjfaOnvqviXQsSkMXA7qOnnT5M8ydvBgjnbyGkXHYi2S37XRwLk9JGJg1PRjP1tM6Pi4ZC00c3I4fsf4');
 const Payment = () => {
     const { id } = useParams()
+    const url = `http://localhost:5000/booking/${id}`
+    const { data: appointment, isLoading } = useQuery(['booking', id], () => fetch(url, {
+        method: 'GET',
+        headers: {
+            authorization: `Bearer ${localStorage.getItem('accessToken')}`
+        }
+    }).then(res => res.json()))
+    if (isLoading) {
+        return <Loading></Loading>
+    }
     return (
         <div>
-            <h2>payment id {id}</h2>
+            <div className="card w-50 max-w-md bg-base-100 shadow-xl my-12">
+                <div className="card-body">
+                    <h2 className='card-title text-success font-bold'>Hello,  {appointment.patientName}</h2>
+                    <h2>Please pay for {appointment.treatment}</h2>
+                    <p>Your appointment: <span className='text-orange-600'>{appointment.date}</span> on {appointment.slot}</p>
+                    <p>Please Pay: ${appointment.price}</p>
+                </div>
+            </div>
+            <div className="card flex-shrink-0 w-50 max-w-md shadow-xl bg-base-100 ">
+                <div className="card-body">
+                    <Elements stripe={stripePromise}>
+                        <CheckoutForm appointment={appointment}/>
+                    </Elements>
+                </div>
+            </div>
         </div>
     );
 };
